8d252da02caa474ff32236a3e345f6b8e17ce84e
Foxtrot <mfaizanasad@gmail.com>
2019-06-16 23:11:49 +0300

    local work

---
+++ Magic_ball_8_game.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 16 23:03:37 2019
+
+@author: Faizan
+"""
+
+# Import the modules
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = input("Ask the magic 8 ball a question: (press enter to quit) ")
+    
+    answers = random.randint(1,8)
+    
+    if question == "":
+        sys.exit()
+    
+    elif answers == 1:
+        print ("It is certain")
+    
+    elif answers == 2:
+        print ("Outlook good")
+    
+    elif answers == 3:
+        print ("You may rely on it")
+    
+    elif answers == 4:
+        print ("Ask again later")
+    
+    elif answers == 5:
+        print ("Concentrate and ask again")
+    
+    elif answers == 6:
+        print ("Reply hazy, try again")
+    
+    elif answers == 7:
+        print ("My reply is no")
+    
+    elif answers == 8:
+        print ("My sources say no")
